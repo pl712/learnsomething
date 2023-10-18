a86e9e8031883f87f13dc40b5981fd5121034f8a
Peter L <85031672+pl712@users.noreply.github.com>
2023-10-18 16:40:35 -0500

    sqrt of number

--- prime.py
+++ prime.py
@@ -1,24 +1,43 @@
 import timeit
+import math
 
+# def disasterCode():
+#     for i in range (2,2500):
+#         uniquePrimes = []
+#         currentPrime = i
+#         for j in range (2,i):
+#             checkPrime = j
+#             flag = False
+#             for k in range (2,checkPrime-1):
+#                 if (j%k==0):
+#                     flag = True
+#                     break
+#             if not flag and i%checkPrime==0 and checkPrime <= i:
+#                 while (currentPrime%checkPrime==0):
+#                     currentPrime/=checkPrime
+#                 uniquePrimes.append(checkPrime)
+#         if len(uniquePrimes) == 0:
+#             uniquePrimes.append(i)
+#         print(uniquePrimes)
+
+#this runs in 0.006 seconds
 def disasterCode():
-    for i in range (2,2500):
-        uniquePrimes = []
-        currentPrime = i
-        for j in range (2,i):
-            checkPrime = j
-            flag = False
-            for k in range (2,checkPrime-1):
-                if (j%k==0):
-                    flag = True
-                    break
-            if not flag and i%checkPrime==0 and checkPrime <= i:
-                while (currentPrime%checkPrime==0):
-                    currentPrime/=checkPrime
-                uniquePrimes.append(checkPrime)
-        if len(uniquePrimes) == 0:
-            uniquePrimes.append(i)
+    for i in range(2, 2500):
+        uniquePrimes = set()
+        
+        n = i
+        while n % 2 == 0:
+            uniquePrimes.add(2)
+            n //= 2
+        for factor in range(3, int(math.sqrt(n)) + 1, 2):  
+            while n % factor == 0:
+                uniquePrimes.add(factor)
+                n //= factor
+        if n > 2:
+            uniquePrimes.add(n)
+        print(uniquePrimes)
+#         
 
-# Benchmark the code
 if __name__ == "__main__":
     benchmark_code = "disasterCode()"
     setup_code = "from __main__ import disasterCode"
